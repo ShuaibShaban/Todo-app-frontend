0966c41cfbe81a9336ac1f1c64d9f4524f030f19
shuaib shaban <shuaib>
2023-03-27 02:50:37 +0300

    read me

--- todo/src/App.js
+++ todo/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import TodoList from './components/TodoList'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
-// import LoginForm from './components/LoginForm';
+import LoginForm from './components/LoginForm';
 
 
 function App() {
   return (
     <div className="App">
-      {/* <LoginForm /> */}
+      <LoginForm />
       <TodoList />
       
     </div>
